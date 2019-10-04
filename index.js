e84950ca769e4463527fcd5746f9e3b9dc6b856c
Adam Dawkins <adam@dragondrop.uk>
2019-10-04 23:22:52 +0100

    ✅ Server side update

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 import {app} from 'hyperapp';
 import {textarea, div, h2, form, button, p} from '@hyperapp/html';
 import {preventDefault, targetValue} from '@hyperapp/events';
+import {Http} from 'hyperapp-fx';
+
 // UTILITIES
 const pluralize = word => word.concat('s');
 const capitalize = word => word.charAt(0).toUpperCase() + word.slice(1);
 
+//    omit :: (k, {k: v}) -> {k: v}
+const omit = (k, obj) =>
+  Object.fromEntries(Object.entries(obj).filter(([key, _]) => key !== k));
 
-// Helpers
-const nextNoteId = state => Object.keys(state.notes).length + 1;
+// Events
+const Create = (state, [resourceName, resource]) => [
+  state,
+  // Optimistic state update causing problems when we get the real thing from the server
+  // AddResourceToState(state, [
+  //   resourceName,
+  //   {...resource, id: nextId(state, resourceName)},
+  // ]),
+  Http({
+    url: `http://localhost:3000/${pluralize(resourceName)}`,
+    options: {
+      method: 'POST',
+      body: `{ "${resourceName}": ${JSON.stringify(resource)} }`,
+      headers: {'Content-Type': 'application/json'},
+    },
+    action(state, response) {
+      return AddResourceToState(state, [resourceName, response]);
+    },
+    error(state, err) {
+      return RemoveResourceFromState(state, [
+        resourceName,
+        lastId(state, resourceName),
+      ]);
+    },
+  }),
+];
+
+const nextId = (state, resourceName) =>
+  Object.keys(state[pluralize(resourceName)]).length + 1;
+const lastId = (state, resourceName) =>
+  Object.keys(state[pluralize(resourceName)]).length;
 
 // Actions
 
@@ -16,30 +50,38 @@ const setNoteContent = (state, content) => ({
   newNote: {content},
 });
 
-const CreateNote = state => ({
+const AddResourceToState = (state, [resourceName, resource]) => ({
   ...state,
-  notes: {
-    ...state.notes,
-    [nextNoteId(state)]: {...state.newNote, createdAt: new Date()},
+  [pluralize(resourceName)]: {
+    ...state[pluralize(resourceName)],
+    [resource.id]: resource,
   },
-  newNote: {},
+  [`new${capitalize(resourceName)}`]: {},
 });
 
+const RemoveResourceFromState = (state, [resourceName, resourceId]) => ({
+  ...state,
+  [pluralize(resourceName)]: omit(
+    resourceId.toString(),
+    state[pluralize(resourceName)],
+  ),
+});
+
+const CreateNote = state => [Create, ['note', state.newNote]];
 // VIEWS
 
-const Note = ([ id, note ]) => div({class: 'note'}, [h2(id), p(note.content)]);
+const Note = ([id, note]) => div({class: 'note'}, [h2(id), p(note.content)]);
 
 app({
   init: {
     newNote: {},
     notes: {
-      1: {content: 'Happy New Year!', createdAt: new Date(2019, 1, 1)},
-      2: {content: 'Merry Christmas', createdAt: new Date(2019, 12, 25)},
+      1: {content: 'Happy New Year!', created_at: new Date(2019, 1, 1)},
+      2: {content: 'Merry Christmas', created_at: new Date(2019, 12, 25)},
     },
   },
   view: state => {
     console.log({state});
-
     return div({}, [
       form({onsubmit: preventDefault(CreateNote)}, [
         textarea({
