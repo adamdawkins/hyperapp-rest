df68515ede326ba31c3ea62e83e0a4c6132d8587
Adam Dawkins <adam@dragondrop.uk>
2019-10-06 23:10:17 +0100

    ✅ Get collection into state dynamically

--- index.js
+++ index.js
@@ -3,15 +3,71 @@ import {textarea, div, h2, form, button, p} from '@hyperapp/html';
 import {preventDefault, targetValue} from '@hyperapp/events';
 import {Http} from 'hyperapp-fx';
 
-const SERVER_URL = 'https://intense-plateau-18988.herokuapp.com';
-
 // UTILITIES
-const pluralize = word => word.concat('s');
+
 const capitalize = word => word.charAt(0).toUpperCase() + word.slice(1);
+
+// Currying from https://gist.github.com/branneman/4ffb7ec3fc4a2091849ba5d56742960c
+const curryN = (fn, arity, accIn = []) => (...args) => {
+  const len = args.length;
+  const accOut = accIn.concat(args);
+  if (len + accIn.length >= arity) {
+    return fn.apply(this, accOut);
+  }
+  return curryN(fn, arity, accOut);
+};
+
+const curry = fn => curryN(fn, fn.length);
 
 //    omit :: (k, {k: v}) -> {k: v}
 const omit = (k, obj) =>
   Object.fromEntries(Object.entries(obj).filter(([key, _]) => key !== k));
+
+const pluralize = word => (word.endsWith('s') ? word : word.concat('s'));
+
+const set = curry((prop, value, source) => {
+  var result = {},
+    i;
+  for (i in source) result[i] = source[i];
+  result[prop] = value;
+
+  return result;
+});
+
+const REST = {
+  init(state, {resources, endpoint}) {
+    window.HYPERAPP_REST = {
+      resources,
+      endpoint,
+    };
+    return state;
+  },
+};
+
+REST.fx = {};
+REST.actions = {};
+
+REST.fx.index = resourceName => {
+  console.log('REST.fx.index', {resourceName});
+  return [
+    [],
+    Http({
+      url: `${HYPERAPP_REST.endpoint}/${pluralize(resourceName)}`,
+      options: {
+        headers: {'Content-Type': 'application/json'},
+      },
+      action: REST.actions.UpdateCollection(pluralize(resourceName)),
+    }),
+  ];
+};
+
+REST.collection = REST.fx.index;
+
+REST.actions.UpdateCollection = curry((resourceName, state, resources) =>
+  set(resourceName, resources, state),
+);
+
+// const SERVER_URL = 'https://intense-plateau-18988.herokuapp.com';
 
 // Events
 const Create = (state, [resourceName, resource]) => [
@@ -70,29 +126,36 @@ const RemoveResourceFromState = (state, [resourceName, resourceId]) => ({
 });
 
 const CreateNote = state => [Create, ['note', state.newNote]];
-// VIEWS
 
-const Note = ([id, note]) => div({class: 'note'}, [h2(id), p(note.content)]);
+// VIEWS
+const Note = note => div({class: 'note'}, [h2(note.id), p(note.content)]);
+
+const resources = ['note'];
+
+const initialState = [
+  REST.init(
+    {title: 'My notes'},
+    {
+      resources: {notes: ['collection', 'create']},
+      endpoint: 'http://localhost:3000',
+    },
+  ),
+  REST.collection('notes'),
+];
 
 app({
-  init: {
-    newNote: {},
-    notes: {
-      1: {content: 'Happy New Year!', created_at: new Date(2019, 1, 1)},
-      2: {content: 'Merry Christmas', created_at: new Date(2019, 12, 25)},
-    },
-  },
+  init: initialState,
   view: state => {
     console.log({state});
     return div({}, [
       form({onsubmit: preventDefault(CreateNote)}, [
         textarea({
           oninput: [setNoteContent, targetValue],
-          value: state.newNote.content,
+          // value: state.newNote.content,
         }),
         button('Create Note'),
       ]),
-      Object.entries(state.notes).map(Note),
+      state.notes && state.notes.map(Note),
     ]);
   },
   node: document.getElementById('app'),
